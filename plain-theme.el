7c376f5bf9d653bf12e414176284736cbdd19108
Yegor Timoshenko <yegortimoshenko@gmail.com>
2017-06-25 21:00:13 +0300

    Improve readability (includes changes to private API)

--- plain-theme.el
+++ plain-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Yegor Timoshenko <yegortimoshenko@gmail.com>
 ;; URL: https://github.com/yegortimoshenko/plain-theme
-;; Version: 6
+;; Version: 7
 
 ;; Permission to use, copy, modify, and/or distribute this software for any
 ;; purpose with or without fee is hereby granted, provided that the above
@@ -20,49 +20,52 @@
 
 ;;; Code:
 
-(deftheme plain "Plain theme without syntax highlighting")
+(deftheme plain "Plain theme without syntax highlighting.")
 
 (defgroup plain-theme nil
-  "Plain theme colors and faces"
+  "Plain theme colors and faces."
   :group 'faces
   :prefix "plain-")
 
 (defcustom plain-background "white"
-  "Color to use for background"
+  "Color to use for background."
   :type 'color)
 
 (defcustom plain-foreground "black"
-  "Color to use for text"
+  "Color to use for text."
   :type 'color)
 
 (defcustom plain-faces '(cursor default eshell-prompt fringe minibuffer-prompt)
-  "List of faces to decolorize"
+  "List of faces to decolorize."
   :type '(repeat symbol))
+
+(defun plain--spec (face)
+  "Return spec for a FACE."
+  `(,face ((t (:background ,plain-background :foreground ,plain-foreground)))))
+
+(defun plain--add (faces)
+  "Add FACES to the theme definition."
+  (apply 'custom-theme-set-faces 'plain (mapcar 'plain--spec faces)))
+
+(plain--add plain-faces)
 
 (defcustom plain-prefix-alist
   '((font-lock . "font-lock-")
     (sh-script . "sh-")
     (web-mode . "web-mode-"))
   "Mapping from files to face prefixes: when file is first loaded,
-decolorizes all faces that start with the prefix"
+decolorizes every face that starts with the prefix."
   :type '(alist :key-type symbol :value-type string))
 
 (require 'cl-lib)
 
-(defun plain--face-list (prefix)
+(defun plain--prefix (prefix)
+  "Return all faces that start with PREFIX."
   (cl-remove-if-not (lambda (s) (string-prefix-p prefix (symbol-name s)))
 		    (face-list)))
 
-(defun plain--face-spec (face)
-  `(,face ((t (:background ,plain-background :foreground ,plain-foreground)))))
-
-(defun plain--set-faces (faces)
-  (apply 'custom-theme-set-faces 'plain (mapcar 'plain--face-spec faces)))
-
-(plain--set-faces plain-faces)
-
 (dolist (a plain-prefix-alist)
-  (eval-after-load (car a) `(plain--set-faces (plain--face-list ,(cdr a)))))
+  (eval-after-load (car a) `(plain--add (plain--prefix ,(cdr a)))))
 
 ;;;###autoload
 (when load-file-name
